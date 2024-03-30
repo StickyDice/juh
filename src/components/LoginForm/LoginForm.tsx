330a2985d3317a3e3afe2af6c41b4a15926809c1
Igor Lekomtsev <leckomtsevigor@yandex.ru>
2024-03-30 17:40:49 +0300

    new config for eslint added, refactored login page

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { Button, Card, Link, Stack, TextField } from "@mui/material";
+import { Box, Button, Card, Link, Stack, TextField, Typography } from "@mui/material";
+import styles from "./login-form.module.css";
 
 interface LoginFormProps {}
 
@@ -20,32 +21,45 @@ export default function LoginForm({}: LoginFormProps) {
   });
 
   return (
-    <Card sx={{padding: 2}}>
+    <Card className={styles.loginFormContainer} sx={{ boxShadow: 3 }}>
+      <Typography className={styles.mainTitle} component="h1" variant="h4" sx={{ marginBottom: 1 }}>
+        {isNewby ? "Регистрация" : "Вход"}
+      </Typography>
       <Stack direction="column" spacing={2} component="form">
-        
-        <Link onClick={() => setIsNewby(!isNewby)} sx={{cursor: "pointer", textAlign: "right"}}>
-          {isNewby ? "я уже зарегистрирован" : "я тут впервые"}
-        </Link>
+        <TextField
+          label="почта"
+          value={formData.email}
+          onChange={(e) => setFormData((d) => ({ ...d, email: e.target.value }))}
+        />
 
-        <TextField label="почта" value={formData.email}
-          onChange={(e) =>
-            setFormData((d) => ({ ...d, email: e.target.value }))
-          } />
+        <TextField
+          label="пароль"
+          value={formData.password}
+          onChange={(e) => setFormData((d) => ({ ...d, password: e.target.value }))}
+          type="password"
+        />
 
-        <TextField label="пароль" value={formData.password}
-          onChange={(e) =>
-            setFormData((d) => ({ ...d, password: e.target.value }))
-          } type="password" />
-          
-          {isNewby && <TextField label="подтверждение пароля" value={formData.confirm_password}
-          onChange={(e) =>
-            setFormData((d) => ({ ...d, confirm_password: e.target.value }))
-          } type="password" /> }
-        
-        <Button type="submit" >
+        {isNewby && (
+          <TextField
+            label="подтверждение пароля"
+            value={formData.confirm_password}
+            onChange={(e) => setFormData((d) => ({ ...d, confirm_password: e.target.value }))}
+            type="password"
+          />
+        )}
+
+        <Button type="submit" variant="contained">
           Войти
         </Button>
 
+        <Box>
+          <Typography>
+            {isNewby ? "Уже имеете аккаунт?" : "Впервые у нас?"}
+            <Link onClick={() => setIsNewby(!isNewby)} className={styles.moveToSignUpOrSignInLink}>
+              {isNewby ? "Войти" : "Зарегистрироваться"}
+            </Link>
+          </Typography>
+        </Box>
       </Stack>
     </Card>
   );
